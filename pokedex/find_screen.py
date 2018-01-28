948284946d37c7be68e62c22c1baa04a3b74f3ab
CheShianHung <hungc@bgsu.edu>
2018-01-28 16:10:24 -0500

    pokedex - perspective warping feature added to find_screen script

--- pokedex/find_screen.py
+++ pokedex/find_screen.py
@@ -9,6 +9,9 @@ ap.add_argument("-q", "--query", required = True, help = "Path to the query imag
 args = vars(ap.parse_args())
 
 image = cv2.imread(args["query"])
+h, w, c = image.shape
+if w > h:
+   image = imutils.rotate_bound(image, -90)
 ratio = image.shape[0]/300.0
 orig = image.copy()
 image = imutils.resize(image, height = 300)
@@ -34,6 +37,47 @@ for c in cnts:
     #cv2.waitKey(0)
 
 cv2.drawContours(image, [screenCnt], -1, (0, 255, 0), 3)
-cv2.imshow("Game Boy Screen", image)
-cv2.waitKey(0)
 
+pts = screenCnt.reshape(4, 2)
+rect = np.zeros((4, 2), dtype = "float32")
+
+s = pts.sum(axis = 1)
+rect[0] = pts[np.argmin(s)]
+rect[2] = pts[np.argmax(s)]
+
+d = np.diff(pts, axis = 1)
+rect[1] = pts[np.argmin(d)]
+rect[3] = pts[np.argmax(d)]
+
+rect *= ratio
+
+(tl, tr, br, bl) = rect
+widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
+widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
+
+heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
+heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
+
+maxWidth = max(int(widthA), int(widthB))
+maxHeight = max(int(heightA), int(heightB))
+
+dst = np.array([
+    [0, 0],
+    [maxWidth - 1, 0],
+    [maxWidth - 1, maxHeight - 1],
+    [0, maxHeight - 1]], dtype = "float32")
+
+M = cv2.getPerspectiveTransform(rect, dst)
+warp = cv2.warpPerspective(orig, M, (maxWidth, maxHeight))
+
+warp = cv2.cvtColor(warp, cv2.COLOR_RGB2GRAY)
+warp = exposure.rescale_intensity(warp, out_range = (0, 255))
+
+(h, w) = warp.shape
+(dX, dY) = (int(w * 0.35), int(h * 0.4))
+crop = warp[30:dY, w - dX:w - 30]
+
+cv2.imwrite("cropped.png", crop)
+
+cv2.imshow("crop", imutils.resize(crop, height = 300))
+cv2.waitKey(0)
